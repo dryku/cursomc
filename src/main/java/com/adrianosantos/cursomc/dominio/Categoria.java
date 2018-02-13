8d27530099a47ffd1023502ce29ee4d0d74223f3
Dryku <dryku@hotmail.com>
2018-02-13 02:02:56 -0200

    Teste a primeira classe de dominio com Rest

---
+++ src/main/java/com/adrianosantos/cursomc/dominio/Categoria.java
@@ -0,0 +1,67 @@
+package com.adrianosantos.cursomc.dominio;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer idcategoria; 
+	private String nmcategoria;
+	
+	public Categoria() {};
+	
+
+	public Categoria(Integer idcategoria, String nmcategoria) {
+		super();
+		this.idcategoria = idcategoria;
+		this.nmcategoria = nmcategoria;
+	}
+
+	public Integer getIdcategoria() {
+		return idcategoria;
+	}
+
+	public void setIdcategoria(Integer idcategoria) {
+		this.idcategoria = idcategoria;
+	}
+
+	public String getNmcategoria() {
+		return nmcategoria;
+	}
+
+	public void setNmcategoria(String nmcategoria) {
+		this.nmcategoria = nmcategoria;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idcategoria == null) ? 0 : idcategoria.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (idcategoria == null) {
+			if (other.idcategoria != null)
+				return false;
+		} else if (!idcategoria.equals(other.idcategoria))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Categorias [idcategoria=" + idcategoria + ", nmcategoria=" + nmcategoria + "]";
+	}
+	
+}
--- src/main/java/com/adrianosantos/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/adrianosantos/cursomc/resources/CategoriaResources.java
@@ -1,8 +1,13 @@
 package com.adrianosantos.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.adrianosantos.cursomc.dominio.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,8 +15,16 @@ public class CategoriaResources {
 	
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest esta Funcionando!!";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Categoria 1");		
+		Categoria cat2 = new Categoria(2, "Categoria 2");		
+
+		List<Categoria> listacat = new ArrayList<>();
+		listacat.add(cat1);
+		listacat.add(cat2);
+//		return "Rest esta Funcionando!!";
+		return listacat;		
 	}
 	
 
